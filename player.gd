3b5b3119a34083f0fe40e05fa9a4fca5236ef8a1
Garnet DeGelder <garnet@garnetdg.ca>
2023-12-29 16:38:53 -0500

    Add basic touch support

--- player.gd
+++ player.gd
@@ -12,10 +12,30 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	process_input(delta)
+	input_frame(delta)
 
-func process_input(delta):
+func _input(event):
+	input_event(event)
+
+var touch_input_origin = null
+var touch_drag_vector = Vector2(0, 0)
+
+func input_event(event):
+	if event is InputEventScreenTouch:
+		if event.pressed:
+			# touch start
+			touch_input_origin = event.position
+		else:
+			# touch end
+			touch_input_origin = null
+			touch_drag_vector = Vector2(0, 0)
+	if event is InputEventScreenDrag:
+		touch_drag_vector = (event.position - touch_input_origin).normalized()
+
+func input_frame(delta):
 	var input = Vector3(0, 0, 0)
+	input.y += touch_drag_vector.x;
+	input.x += touch_drag_vector.y;
 	if Input.is_action_pressed("up"):
 		input.x -= 1
 	if Input.is_action_pressed("down"):
